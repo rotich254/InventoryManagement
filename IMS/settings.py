be8f89c9c3550ffaba650f47a94433f33c2fa477
rotich254 <rotichk254@gmail.com>
2023-09-15 08:01:51 +0300

    new commit

--- IMS/settings.py
+++ IMS/settings.py
@@ -141,9 +141,3 @@ STATIC_ROOT = (BASE_DIR/"asert/")
 
 LOGIN_URL = 'user-login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rotichwebsite@gmail.com'
-EMAIL_HOST_PASSWORD = '@rotich254'
